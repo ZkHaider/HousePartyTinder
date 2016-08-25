1f8dba065f91fb714332d44a8c09e8597f2586d4
ZkHaider <hkhan@oglethorpe.edu>
2016-08-24 19:20:42 -0700

    Added a login layout

---
+++ app/src/main/java/co/housepartytinder/interactors/ILoginInteractor.java
@@ -0,0 +1,10 @@
+package co.housepartytinder.interactors;
+
+/**
+ * Created by ZkHaider on 8/24/16.
+ */
+
+public interface ILoginInteractor {
+
+
+}
---
+++ app/src/main/java/co/housepartytinder/interactors/MainInteractor.java
@@ -0,0 +1,8 @@
+package co.housepartytinder.interactors;
+
+/**
+ * Created by ZkHaider on 8/24/16.
+ */
+
+public class MainInteractor {
+}
